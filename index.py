d0e4529a8b45d0dfcda674953709f7a2949e39f5
Iago <ogaidsa@gmail.com>
2023-02-19 12:51:59 -0300

    index

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 import discord
 import functions.commands as commands
 
-bot_token = 'MTA2NjM2MDgzOTI1MjIzNDMxMA.Gu5_Xt.uY_TX4KUFVESwPD6YUIksEfOS7MpuR9guDzQZs'
+bot_token = ' YOUR TOKEN '
 prefix = '$'
 
 class MyClient(discord.Client):
